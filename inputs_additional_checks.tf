4b23adffb42566a674be0723caf0cfaf2b2bb6e5
graham-fletcher-appsbroker <g.p.fletcher@me.com>
2024-11-22 08:22:22 +0000

    add checks

--- inputs_additional_checks.tf
+++ inputs_additional_checks.tf
@@ -5,7 +5,7 @@ check "input_checks" {
     }
 
     assert {
-        condition = var.bootstrap_repo != null and var.github_app_intilation_id == null
+        condition = var.bootstrap_repo != null && var.github_app_intilation_id == null
         error_message = "A bootstrap repo cannot be defined until the github integration is complete and the integration id provided in input github_app_intigration_id"
     }
 }

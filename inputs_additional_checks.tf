8331340196185d4c6b634194be84d9d97b42b7b1
graham-fletcher-appsbroker <g.p.fletcher@me.com>
2024-11-22 08:14:28 +0000

    add checks

--- inputs_additional_checks.tf
+++ inputs_additional_checks.tf
@@ -1,4 +1,4 @@
-check "input checks" {
+check "input_checks" {
     assert {
         condition = (startswith(var.root_location,"folder/")) || (var.root_name != null)
         error_message = "Cannot be deployed in the root of an org without defining a top level folder"

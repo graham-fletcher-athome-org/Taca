ffc2117ae2581a190ab986ddf1272305c0c1d0dd
graham-fletcher-appsbroker <g.p.fletcher@me.com>
2024-11-22 08:21:34 +0000

    add checks

--- inputs_additional_checks.tf
+++ inputs_additional_checks.tf
@@ -3,4 +3,9 @@ check "input_checks" {
         condition = (startswith(var.root_location,"folder/")) || (var.root_name != null)
         error_message = "Cannot be deployed in the root of an org without defining a top level folder"
     }
+
+    assert {
+        condition = var.bootstrap_repo != null and var.github_app_intilation_id == null
+        error_message = "A bootstrap repo cannot be defined until the github integration is complete and the integration id provided in input github_app_intigration_id"
+    }
 }

6781e3d943822e131efadfc214f1d9341702405b
graham-fletcher-appsbroker <g.p.fletcher@me.com>
2024-12-05 14:00:08 +0000

    refactor

--- iam/folder_iam_member.tf
+++ iam/folder_iam_member.tf
@@ -1,6 +1,6 @@
 locals{
     is_folder_iam_member = (var.target.type=="f")
-    iam_apply = local.is_folder_iam_member ? local.iam_map : {}
+    iam_apply = local.is_folder_iam_member ? local.iam_dict : {}
 }
 resource "google_folder_iam_member" "folder" {
   for_each = local.iam_apply

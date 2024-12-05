8ad7327d0af537260c48532c7b2668487c38ad71
graham-fletcher-appsbroker <g.p.fletcher@me.com>
2024-12-05 13:43:59 +0000

    refactor

--- iam/folder_iam_member.tf
+++ iam/folder_iam_member.tf
@@ -1,13 +1,13 @@
 locals{
     is_folder_iam_member = startswith(var.target,"folders/")
-    iam_apply = local.is_folder_iam_member ? local.iam_dict : {}
+    iam_apply = local.is_folder_iam_member ? local.iam_keys : toset([])
 }
-/*resource "google_folder_iam_member" "folder" {
+resource "google_folder_iam_member" "folder" {
   for_each = local.iam_apply
   folder   = var.target
-  role     = each.value.role
-  member   = format("serviceAccount:%s",each.value.sa)
-}*/
+  role     = local.iam_dict[each.value].role
+  member   = format("serviceAccount:%s",local.iam_dict[each.value].sa)
+}
 
 
 

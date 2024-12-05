387d6bfe42a11a10481fa93ce85c418923184136
graham-fletcher-appsbroker <g.p.fletcher@me.com>
2024-12-05 13:24:02 +0000

    refactor

--- iam/inputs_processing.tf
+++ iam/inputs_processing.tf
@@ -9,17 +9,10 @@ locals{
                         ]
                     ]
                 ]) : format("%s%s",x.name,x.role) => x}
+
+    iam_keys = to_set([for k,v in local.iam_dict:k])
 }
 
 output "joe" {
-    value = {for x in flatten([for binding in var.iam : 
-                    [for builder in binding.builders :
-                        [for role in binding.roles:{
-                            name  = builder.name
-                            sa     = builder.sa.email
-                            role     = role
-                            }
-                        ]
-                    ]
-                ]) : format("%s%s",x.name,x.role) => x}
+    value = local.iam_keys
 }

bcd4ea91967bed650496d67b54c78c5b4075cf49
graham-fletcher-appsbroker <g.p.fletcher@me.com>
2024-12-06 09:44:57 +0000

    refator

--- builder/make_backend.tf
+++ builder/make_backend.tf
@@ -3,11 +3,11 @@ resource "local_file" "backend" {
   content    = <<EOT
 terraform {
   backend "gcs" {
-    bucket  = "${google_storage_bucket.build_assets_buckets["bootstrap"].id}"
+    bucket  = "${google_storage_bucket.build_assets_buckets.id}"
     prefix  = "terraform/state"
   }
 }
 EOT
   filename   = "./backend.autogen.tf"
-  depends_on = [google_storage_bucket.build_assets_bucket]
+  depends_on = [google_storage_bucket.build_assets_buckets]
 }

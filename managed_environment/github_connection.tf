7ee936f2c19ac3aa94b351eef341e8823bbd7812
graham-fletcher-appsbroker <g.p.fletcher@me.com>
2024-12-06 08:33:23 +0000

    refator

--- managed_environment/github_connection.tf
+++ managed_environment/github_connection.tf
@@ -13,7 +13,7 @@ resource "google_secret_manager_secret_version" "github_token_secret_version" {
   secret      = google_secret_manager_secret.github_token_secret.id
   secret_data = var.pac
   lifecycle {
-    ignore_changes = [secret_data]
+    ignore_changes = [secret_data,]
   }
 }
 

65cd565d177ad303588bfaaf9ebdec91a11ba551
graham-fletcher-appsbroker <g.p.fletcher@me.com>
2024-12-11 16:45:12 +0000

    debug

--- bin/plugins/iam_manager/load_inputs.tf
+++ bin/plugins/iam_manager/load_inputs.tf
@@ -26,7 +26,7 @@ locals {
 
 
   config_places_deref = { for key, value in local.config_unpacked : key => {
-    target = var.managed_environment.places[value.target]
+    target = try(var.managed_environment.places[value.target].id, null)
     sa     = value.sa
     role   = value.role
     }

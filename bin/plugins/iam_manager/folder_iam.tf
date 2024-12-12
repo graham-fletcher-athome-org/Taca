238569313c46f7913431fed314498e6e8f09c4d8
graham-fletcher-appsbroker <g.p.fletcher@me.com>
2024-12-12 10:58:34 +0000

    Add iam folders

--- bin/plugins/iam_manager/folder_iam.tf
+++ bin/plugins/iam_manager/folder_iam.tf
@@ -6,7 +6,7 @@
 # 1 "<built-in>" 2
 # 1 "./plugins/iam_manager/folder_iam.tf" 2
 resource "google_folder_iam_member" "folder" {
-  for_each = { for key, value in local.iam_to_apply : key => value if value.target.type == "f") }
+  for_each = { for key, value in local.iam_to_apply : key => value if value.target.type == "f" }
   folder = each.value.target.id
   role = each.value.role
   member = each.value.sa
--- src/plugins/iam_manager/folder_iam.tf
+++ src/plugins/iam_manager/folder_iam.tf
@@ -1,5 +1,5 @@
 resource "google_folder_iam_member" "folder" {
-  for_each = { for key, value in local.iam_to_apply : key => value if value.target.type == "f") }
+  for_each = { for key, value in local.iam_to_apply : key => value if value.target.type == "f" }
   folder   = each.value.target.id
   role     = each.value.role
   member   = each.value.sa
